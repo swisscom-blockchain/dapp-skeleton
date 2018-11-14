6c6b7ca49a12bc7f2e4ed701d16a793eed5dbe4b
= <=>
2018-11-14 16:41:01 +0100

    first dapp

--- frontend/helloWorld.js
+++ frontend/helloWorld.js
@@ -1,9 +1,9 @@
 
-document.write("This text comes from an external script.");
 
 var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 console.log(web3);
 var contractInstance; 
+var eventCounter = 0;
 
 init = function() {
     var address = document.getElementById('address').value;
@@ -14,12 +14,24 @@ init = function() {
     });
 
     contractInstance.events.allEvents(eventCallback);
-    console.dir(contractInstance);
 }
 
 eventCallback = function(error, result) {
-    console.log("error:", error)
-    console.log("result:", result)
+    if(error) console.error("error:", error)
+    if(result) {
+        var from = result.returnValues.from;
+        var event = result.event;
+        var msg = result.returnValues[1];
+        var message = "EventNbr: " + eventCounter++ + ", from: " + from + ", event: " + event + ", msg: " + msg;
+        console.log(message)
+
+        var node = document.createElement("div");
+        node.className = "alert alert-success";
+        var textnode = document.createTextNode(message);
+        node.appendChild(textnode);    
+        var list = document.getElementById("events");
+        list.insertBefore(node, list.childNodes[0]);
+    }
 }
 
 writeMessage = function() {
@@ -45,4 +57,30 @@ readMessage = function() {
 clearMessage = function() {
     console.log("Clear Message Clicked");
     document.getElementById('message').value = "";
+}
+
+
+writeNumber = function() {
+    console.log("Write Number Clicked");
+    var message = document.getElementById('number').value;
+    contractInstance.methods.setNumber(message).send().then(function(receipt){
+        console.log(receipt);
+    });
+}
+
+readNumber = function() {
+    console.log("Read Number Clicked");
+    var message = contractInstance.methods.number().call(function(error, result){
+        var msg;
+        if(error) msg = error;
+        if(result) msg = result;
+        document.getElementById('number').value = msg;
+        console.log("error:", error)
+        console.log("result:", result)
+    });
+}
+
+clearNumber = function() {
+    console.log("Clear Number Clicked");
+    document.getElementById('number').value = "";
 }
