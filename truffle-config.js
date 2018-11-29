256f19849fb2801ff8b2b332d12448b482d1e6bb
= <=>
2018-11-29 14:28:58 +0100

    adding network to truffle.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,19 @@
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+var privKeyrinkeby = "<YOUR-PRIVATE-KEY>";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
-      gas: 4000000
-    }
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new PrivateKeyProvider(privKeyrinkeby, "https://rinkeby.infura.io/v3/0cd25f13fa42452181039a22154c025a"),
+      gasPrice: 50000000000, // 50 gwei,
+      gas: 4600000,
+      network_id: 4,
+    },
   },
   solc: {
     optimizer: {
