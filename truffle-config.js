958f8b9a3d646a3d3dc9ade8ed1ec78e7bbd59cc
= <=>
2019-03-12 09:56:13 +0100

    fix hdWalletProvider

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
+// Required for Rinkeby deployment 
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var privateKey = "<YOUR PRIVATE KEY HERE>";
+var privateKey = "<your private key here>";
 var rinkebyEndpoint = "https://rinkeby.infura.io/v3/0cd25f13fa42452181039a22154c025a";
-var provider = new HDWalletProvider(privateKey, rinkebyEndpoint);
 
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     rinkeby: {
-      provider: () => provider,
+      provider: () => new HDWalletProvider(privateKey, rinkebyEndpoint),
       gasPrice: 50000000000, // 50 gwei,
       gas: 4600000,
       network_id: 4,
